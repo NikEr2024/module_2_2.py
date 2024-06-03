6004eaac3ae9964e924d99b5be0ddd92c281a608
NikEr2024 <n.erdyneev2000@gmail.com>
2024-06-03 23:49:47 +0300

    Add files via upload

---
+++ module 2.2.py
@@ -0,0 +1,34 @@
+print('Введите Ваши числа')
+
+first = int(input(''))
+second = int(input(''))
+third = int(input(''))
+
+if first == second == third:
+   print('Повторений "3"')
+
+elif first != second != third != first:
+   print('Повторений "0"')
+
+else:
+   print('Повторений "2"')
+
+
+
+
+
+
+
+#(first != second == third or first != third == second):
+  # print('Повторений "2"')
+
+#elif first == second == third:
+ #  print('Повторений "3"')
+
+#elif (first == second == third or first != second == third or first == second != third):
+  # print('Повторений "2"')
+
+#elif first != second != third:
+  # print('Повторений "0"')
+
+
